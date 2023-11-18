dd3d88d348a9ea7b27198c89a02a6ee534b6d595
amine0110 <mokhtari.amine1996@gmail.com>
2023-11-18 12:42:42 +0100

    calculate speed in secondes

---
+++ stand_up_down_speed.py
@@ -0,0 +1,66 @@
+import cv2
+import mediapipe as mp
+
+# Initialize MediaPipe Pose model
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+mp_drawing = mp.solutions.drawing_utils
+
+# Start capturing video from the webcam
+cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture('./test_videos/left_right.mp4')
+
+is_person_standing = False
+was_person_standing = False
+speed = 0
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    results = pose.process(frame_rgb)
+
+    if results.pose_landmarks:
+        mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        landmarks = results.pose_landmarks.landmark
+        shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y,
+                    landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+        hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y,
+               landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+        ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y,
+                 landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+        threshold = 0.1 
+
+        if abs(max(shoulder) - min(ankle)) < 0.3:
+            speed += 1
+        else:
+            speed = 0
+        
+        # Check if these points are roughly horizontally aligned (lying down)
+        if abs(max(shoulder) - min(ankle)) < threshold:  
+            is_person_standing = False
+        else:
+            is_person_standing = True
+
+        # Detect transition from standing to lying down
+        if was_person_standing and not is_person_standing:
+            print("The person has transitioned from standing to lying down.")
+            if abs(max(shoulder) - min(ankle)) < 0.1:
+                print("The speed in secondes: ", speed * .03)
+
+            was_person_standing = False
+        elif is_person_standing:
+            was_person_standing = True
+
+    cv2.imshow('MediaPipe Pose', frame)
+
+    if cv2.waitKey(1) & 0xFF == 27:  # Exit with ESC key
+        break
+
+cap.release()
+cv2.destroyAllWindows()
